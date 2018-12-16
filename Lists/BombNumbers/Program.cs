acd94b1d31ca83d6ed94b80a60ccf2eeb0e23cea
keskineva <37533789+keskineva@users.noreply.github.com>
2018-12-16 13:39:11 +0200

    added problems

---
+++ Lists/BombNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int bombNum = input[0];
+            int power = input[1];
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == bombNum)
+                {
+                    int leftindex = Math.Max(i - power, 0);
+                    int rightindex = Math.Min(i + power, numbers.Count - 1);
+
+                    numbers.RemoveRange(i, rightindex - i);
+                    numbers.RemoveAt(i);
+                    numbers.RemoveRange(leftindex, i - leftindex);
+                    i = 0;
+                }
+
+            }
+            Console.WriteLine(string.Join("", numbers.Sum()));
+            //Console.WriteLine("{0} {1}", position , lenght);
+
+        }
+    }
+}
---
+++ Lists/SumReversedNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumReversedNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            int sum = 0;
+
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i].ToString();
+                int newNum = numbers[i].ToString().Reverse().Aggregate(0, (b, x) => 10 * b + x - '0');
+                sum += newNum;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
